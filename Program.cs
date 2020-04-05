c380682301ee359c62c0fb713ee89572b69af73d
Ramz Nazarov <Ramzi.13.09.18@gmail.com>
2020-04-05 06:23:09 +0300

    create project for HomeWork04.04

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork04._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
